125619f3933576285a022cce4d658a9097e6ed7d
xxx1720 <53284556+xxx1720@users.noreply.github.com>
2019-12-16 10:45:11 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ using namespace jsonparser;
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
-    std::cout << argv[0] << " <filename> [-f]";
+    std::cout << argv[0] << " <filename> [-f]\n";
     return 0;
   }
 
